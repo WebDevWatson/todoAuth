06494d1d70da80c645a53148fe7f26f901538290
Garry <Watson.WebDev@outlook.com>
2022-09-06 18:04:22 +0100

    Altered logout in in controller/auth, possible fix for logout error

--- controllers/auth.js
+++ controllers/auth.js
@@ -41,13 +41,13 @@ const User = require('../models/User')
     // req.logout() --- Passport update, this is now async
     req.logout(function(err) {
       if (err) { return next(err); }
-      // res.redirect('/') --- causes error later with the session destroy
+      res.redirect('/') //--- causes error later with the session destroy
     })
-    req.session.destroy((err) => {
-      if (err) console.log('Error : Failed to destroy the session during logout.', err)
-      req.user = null
-      res.redirect('/')
-    })
+    // req.session.destroy((err) => {
+    //   if (err) console.log('Error : Failed to destroy the session during logout.', err)
+    //   req.user = null
+    //   res.redirect('/')
+    // })
   }
   
   exports.getSignup = (req, res) => {
